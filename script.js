af2d5b6f8d25f8cc5a6a7edc6676491cc676f9cf
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-08-03 16:25:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -398,10 +398,10 @@ function init () {
                 a.click();
                 
             }
-        })
+        });
     }
     
-    var x = window.matchMedia("(max-width: 700px)")
+    var x = window.matchMedia("(max-width: 768px)")
     myFunction(x) // Call listener function at run time
     x.addListener(myFunction) // Attach listener function on state changes
     // CALLING THE IMAGE UPLOAD
